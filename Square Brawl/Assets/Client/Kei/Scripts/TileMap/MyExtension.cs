e64943a03b96098ed6fc5cf8f010d539fd7250c8
cingjia <news2000tw@gmail.com>
2021-06-28 19:13:41 +0900

    Grid Editor build/ erase function

---
+++ Square Brawl/Assets/Client/Kei/Scripts/TileMap/MyExtension.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using AutoMapper;  //Require download from Nuget
+
+
+public static class MyExtension
+{
+    public static GameObject GetRayHit2D(Vector2 mouseInput, LayerMask layerMask = default)
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(mouseInput);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 10, layerMask, -1);
+
+        if (hit.collider != null)
+        {
+            //hit.transform.GetComponent<IClickableGameObject>()?.OnClick();
+            return hit.collider.gameObject;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    /*
+    //Require Nuget AutoMapper
+    public static T CloneData<T>(T origin_data)
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<T, T>();
+        });
+        var mapper = config.CreateMapper();
+        return mapper.Map<T>(origin_data);
+    }
+    public static KeyValuePair<string, CardData> CloneDataWithID(string card_id) 
+    {
+        CardData data_clone = new CardData();
+        //提取副本
+        data_clone = MyExtension.CloneData<CardData>(MyExtension.LoadCardData(card_id));
+
+        //給temp id
+        string random_id = System.Guid.NewGuid().ToString("N");
+        data_clone.id = random_id;
+
+        return new KeyValuePair<string, CardData>(random_id, data_clone);
+    }*/
+
+
+    public static Color RandomColor()
+    {
+        Color res = new Color();
+        res.r = Random.Range(0f, 1f);
+        res.g = Random.Range(0f, 1f);
+        res.b = Random.Range(0f, 1f);
+        return res;
+    }
+
+    public static Vector2Int Abs(this Vector2Int v2)
+    {
+        return new Vector2Int(Mathf.Abs(v2.x), Mathf.Abs(v2.y));
+    }    
+
+    //檢查該v2是否在grid範圍內
+    public static bool IsInsideGridRange(this Vector2Int v2, Vector2Int _mapSize)
+    {
+        int grid_width = _mapSize.x;
+        int grid_height = _mapSize.y;
+        if (v2.x >= grid_width || v2.x <= -1 ||
+            v2.y >= grid_height || v2.y <= -1
+            )
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    /*
+    public static bool IsSameSide(this Player.PlayerSide side, Player.PlayerSide _test_side)
+    {
+        int res = (int)side * (int)_test_side;
+        return !(res == -1 || res == 0);//互為敵對相乘=-1  法術卡不算同一邊 //TODO 在文件資料紀錄卡片是否可共用(none)
+    }
+    */
+
+     /*
+    //取得該卡片的所在cell
+    public static Transform GetCellTransform(this TileCell c)
+    {
+        if (c != null)
+            return c.transform.parent;
+        else
+            return null;
+    } */
+    /*
+    //取得該卡片的所在cell
+    public static CellControl GetCell(this Card c)
+    {
+        return c.transform.parent.GetComponent<CellControl>();
+    }*/
+
+}
---
+++ Square Brawl/Assets/Client/Kei/Scripts/TileMap/TileMapManager.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMapManager : MonoBehaviour
+{
+    public static TileMapManager instance;
+    public GameObject cellPrefab;
+
+    public SpriteRenderer firstCell;
+
+    public Vector2Int mapSize = new Vector2Int(10, 5);
+
+    public List<TileCell> gridCells = new List<TileCell>();
+
+
+    public TileCell center_cell { get { return (gridCells[(gridCells.Count - 1) / 2]); } }
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(instance);
+        }
+    }
+    public void Start()
+    {
+        GenerateGrid();
+    }
+    public void GenerateGrid()
+    {
+
+        float halfWidth = firstCell.bounds.size.x / 2;
+        float halfHeight = firstCell.bounds.size.y / 2;
+
+        int _counter = 0;
+        for (int i = 0; i < mapSize.x; i++)
+        {
+            for (int j = 0; j < mapSize.y; j++)
+            {
+                Vector2 offset = new Vector2(i * 2 * halfWidth, j * 2 * halfHeight) + (Vector2)firstCell.transform.position;
+                GameObject cell = Instantiate(cellPrefab, offset, Quaternion.identity, transform);
+                cell.name = "( " + i + " , " + j + " ) ";
+
+                TileCell _cell = cell.GetComponent<TileCell>();
+                _cell.grid_index = _counter;
+
+                gridCells.Add(_cell);
+                _counter++;
+            }
+        }
+    }
+
+    public Vector2Int CellToVector2(Transform cell)
+    {
+        //int index = cell.GetSiblingIndex();
+        if (cell.GetComponent<TileCell>() == null)
+        {
+            return new Vector2Int(99, 99);
+        }
+        int index = cell.GetComponent<TileCell>().grid_index;
+        int x = index / (mapSize.y);
+        int y = index % (mapSize.y);
+        return new Vector2Int(x, y);
+
+    }
+    public Transform Vector2ToCell(Vector2Int vec)
+    {
+        //int index = gridWidth * gridCount * vec.y + vec.x;
+        int index = mapSize.y * vec.x + vec.y;
+
+        if (vec.IsInsideGridRange(mapSize) &&
+            (index < gridCells.Count && index >= 0))
+        {
+            return gridCells[index].transform;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
+    public TileCell[] GetRowCell(int x_from, int x_to, int y)
+    {
+        List<TileCell> res = new List<TileCell>();
+
+        bool virse = false;
+
+        if (x_from > x_to)
+        {
+            //交換頭尾
+            int temp = x_from;
+            x_from = Mathf.Clamp(x_to, 0, 10);
+            x_to = Mathf.Clamp(temp, 0, 10);
+
+            virse = true;
+        }
+
+        for (int i = x_from; i <= x_to; i++)
+        {
+            Vector2Int v2 = new Vector2Int(i, y);
+            Transform cell = Vector2ToCell(v2);
+
+            if (i >= 10 || i < 0 || cell == null)
+                continue;
+            res.Add(cell.GetComponent<TileCell>());
+
+        }
+
+        if (virse)
+        {
+            res.Reverse();
+        }
+
+        return res.ToArray();
+    }
+    public TileCell[] GetColumnCell(int y_from, int y_to, int x)
+    {
+        List<TileCell> res = new List<TileCell>();
+
+        bool virse = false;
+
+        if (y_from > y_to)
+        {
+            //交換頭尾
+            int temp = y_from;
+            y_from = Mathf.Clamp(y_to, 0, 5);
+            y_to = Mathf.Clamp(temp, 0, 5);
+
+            virse = true;
+        }
+
+        for (int i = y_from; i <= y_to; i++)
+        {
+            Vector2Int v2 = new Vector2Int(x, i);
+            Transform cell = Vector2ToCell(v2);
+
+            if (i >= 5 || i < 0 || cell == null)
+                continue;
+            res.Add(cell.GetComponent<TileCell>());
+
+
+        }
+
+        if (virse)
+        {
+            res.Reverse();
+        }
+
+        return res.ToArray();
+    }
+
+
+}
