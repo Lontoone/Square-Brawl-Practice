d14da4f3e312b9f6b8f8a58270a4c12863c69ba6
Sean <sean0990461@gmail.com>
2021-08-10 01:42:50 +0900

    scene amend

---
+++ Square Brawl/Assets/Client/Sean/mockup/RoomLayout.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class RoomLayout : MonoBehaviour
+{
+    [SerializeField] private Transform m_Transform;
+    [SerializeField] private Vector2 m_Spacing;
+    private int m_Index;
+
+    private void Update()
+    {
+        m_Index = m_Transform.childCount;
+        Debug.Log(m_Transform.childCount);
+
+        if (m_Index <= 4)
+        {
+            if (m_Index >= 1)
+            {
+                m_Transform.GetChild(0).localPosition = new Vector3(-m_Spacing.x / 2, m_Spacing.y / 2);
+            }
+            if (m_Index >= 2)
+            {
+                m_Transform.GetChild(1).localPosition = new Vector3(m_Spacing.x / 2, m_Spacing.y / 2);
+            }
+            if (m_Index >= 3)
+            {
+                m_Transform.GetChild(2).localPosition = new Vector3(-m_Spacing.x / 2, -m_Spacing.y / 2);
+            }
+            if (m_Index == 4)
+            {
+                m_Transform.GetChild(3).localPosition = new Vector3(m_Spacing.x / 2, -m_Spacing.y / 2);
+            }
+        }
+        else
+        {
+            Debug.LogError("Player Room List Out of Range");
+        }
+    }
+}
