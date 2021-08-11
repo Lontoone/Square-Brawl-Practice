adfd96353a1081d8cc1cf03fc9db7cf348fd25ab
sean <sean0990461@gmail.com>
2021-08-11 17:01:23 +0900

    room layout amend

--- Square Brawl/Assets/Client/Sean/mockup/RoomLayout.cs
+++ Square Brawl/Assets/Client/Sean/mockup/RoomLayout.cs
@@ -8,7 +8,13 @@ public class RoomLayout : MonoBehaviour
 {
     [SerializeField] private Transform m_Transform;
     [SerializeField] private Vector2 m_Spacing;
+
     private int m_Index;
+    private int player1 = 0;
+    private int player2 = 0;
+    private int player3 = 0;
+    private int player4 = 0;
+
 
     private void Update()
     {
@@ -16,21 +22,25 @@ public class RoomLayout : MonoBehaviour
 
         if (m_Index <= 4)
         {
-            if (m_Index >= 1)
+            if (m_Index >= 1 && player1 == 0)
             {
                 m_Transform.GetChild(0).localPosition = new Vector3(-m_Spacing.x / 2, m_Spacing.y / 2);
+                player1++;
             }
-            if (m_Index >= 2)
+            if (m_Index >= 2 && player2 == 0)
             {
                 m_Transform.GetChild(1).localPosition = new Vector3(m_Spacing.x / 2, m_Spacing.y / 2);
+                player2++;
             }
-            if (m_Index >= 3)
+            if (m_Index >= 3 && player3 == 0)
             {
                 m_Transform.GetChild(2).localPosition = new Vector3(-m_Spacing.x / 2, -m_Spacing.y / 2);
+                player3++;
             }
-            if (m_Index == 4)
+            if (m_Index == 4 && player4 == 0)
             {
                 m_Transform.GetChild(3).localPosition = new Vector3(m_Spacing.x / 2, -m_Spacing.y / 2);
+                player4++;
             }
         }
         else
@@ -38,4 +48,12 @@ public class RoomLayout : MonoBehaviour
             Debug.LogError("Player Room List Out of Range");
         }
     }
+
+    private void OnDisable()
+    {
+         player1 = 0;
+         player2 = 0;
+         player3 = 0;
+         player4 = 0;
+    }   
 }
