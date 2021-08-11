abd440de190560cec860af59da539bda358e64b8
cingjia <news2000tw@gmail.com>
2021-08-11 11:45:35 +0900

    master image

--- Square Brawl/Assets/Client/Tenn/Scripts/Photon/PlayerListItem.cs
+++ Square Brawl/Assets/Client/Tenn/Scripts/Photon/PlayerListItem.cs
@@ -11,8 +11,6 @@ public class PlayerListItem : MonoBehaviourPunCallbacks
     [SerializeField] Text text;
     [SerializeField] Image readyImage;
     [SerializeField] Image masterImage;
-    //[SerializeField] Button readyBtn;
-    //[SerializeField] Button unreadyBtn;
     [SerializeField] Button giveMasterBtn;
 
     public MenuReadyButton readyButton;
@@ -28,79 +26,30 @@ public class PlayerListItem : MonoBehaviourPunCallbacks
         //SetReady(isReady);
         readyButton.Init(_player);
 
-        //readyBtn.onClick.AddListener(delegate { SetReadyBtn(true); });
-        //unreadyBtn.onClick.AddListener(delegate { SetReadyBtn(false); });
         giveMasterBtn.onClick.AddListener(delegate { TransferMaster(); });
 
-        //If this player is not mine => I can't operate his button.
-        /*
-        if (!player.IsLocal)
-        {
-            readyBtn.gameObject.SetActive(false);
-            unreadyBtn.gameObject.SetActive(false);
-        }*/
     }
-    /*
-    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
-    {
-        base.OnPlayerPropertiesUpdate(targetPlayer, changedProps);
 
-        Debug.Log("ready btn update " + targetPlayer.NickName + " " + player.NickName);
-        if (targetPlayer == player)
-        {
-            object _data;
-            if (changedProps.TryGetValue(CustomPropertyCode.READY, out _data))
-            {
-                SetReady((bool)_data);
-            }
-        }
-    }*/
     public override void OnMasterClientSwitched(Player newMasterClient)
     {
         base.OnMasterClientSwitched(newMasterClient);
         SetMasterImage(newMasterClient);       
     }
-    /*
-    public void SetReadyBtn(bool _isReady)
-    {
-        SetReady(_isReady);
-        //int _playerIndex=
-        //LocalDataManager.localPlayers[];
 
-        player.SetCustomProperties(MyPhotonExtension.WrapToHash(new object[] { CustomPropertyCode.READY, _isReady }));
-
-    }*/
-    /*
-    private void SetReady(bool _isReady)
-    {
-        isReady = _isReady;
-        if (isReady)
-        {
-            readyImage.color = Color.green;
-        }
-        else
-        {
-            readyImage.color = Color.white;
-        }
-
-        if (player.IsLocal)
-        {
-            readyBtn.gameObject.SetActive(!_isReady);
-            unreadyBtn.gameObject.SetActive(_isReady);
-        }
-    }*/
     public void SetMasterImage(Player _newMaster)
     {
         if (_newMaster.IsLocal) //check master's
         {
             if (player.IsMasterClient)
             {
-                masterImage.enabled = true;
+                //masterImage.enabled = true;
+                masterImage.gameObject.SetActive(true);
                 giveMasterBtn.gameObject.SetActive(false);
             }
             else
             {
-                masterImage.enabled = false;
+                //masterImage.enabled = false;
+                masterImage.gameObject.SetActive(false);
                 giveMasterBtn.gameObject.SetActive(true);
             }
         }
@@ -108,10 +57,12 @@ public class PlayerListItem : MonoBehaviourPunCallbacks
         {
             if (_newMaster == player)
             {
-                masterImage.enabled = true;
+                //masterImage.enabled = true;
+                masterImage.gameObject.SetActive(true);
             }
             else {
-                masterImage.enabled = false;
+                //masterImage.enabled = false;
+                masterImage.gameObject.SetActive(false);
             }
             giveMasterBtn.gameObject.SetActive(false);
         }
