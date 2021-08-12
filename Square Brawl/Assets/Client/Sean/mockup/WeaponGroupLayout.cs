d80684a056e4e3820ac1597243626aeb71daaace
sean <sean0990461@gmail.com>
2021-08-12 20:23:31 +0900

    scene amend

--- Square Brawl/Assets/Client/Sean/mockup/WeaponGroupLayout.cs
+++ Square Brawl/Assets/Client/Sean/mockup/WeaponGroupLayout.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
 
 public class WeaponGroupLayout : MonoBehaviour
 {
     [SerializeField] private Transform m_GroupParent;
-
+    [SerializeField] private int m_Row;
+    [SerializeField] private int m_Column;
+    [SerializeField] private Vector2 m_Spacing;
+    /*
     void Update()
     {
+        if (m_Column % 2 == 1)
+        { 
         
-    }
+        }
+    }*/
 }
