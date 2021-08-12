975dbe0b96e0dcd17dc33b1a56ebe49f5404f574
sean <sean0990461@gmail.com>
2021-08-12 20:01:25 +0900

    weapon selection amend

---
+++ Square Brawl/Assets/Client/Sean/mockup/WeaponGroupLayout.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponGroupLayout : MonoBehaviour
+{
+    [SerializeField] private Transform m_GroupParent;
+
+    void Update()
+    {
+        
+    }
+}
