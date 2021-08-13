1889ed49d8c3c2c9c183de2b3d1b489b32072727
sean <sean0990461@gmail.com>
2021-08-13 19:29:51 +0900

    scene amend

--- Square Brawl/Assets/Client/Tenn/Scripts/GameScene/WinnerController.cs
+++ Square Brawl/Assets/Client/Tenn/Scripts/GameScene/WinnerController.cs
@@ -8,12 +8,18 @@ public class WinnerController : MonoBehaviour
 {
     public Player player;
     public Image playerColor;
+    public Image winnerIcon;
     public Text playerNameText;
+
+    private Color color;
 
     public void SetWinner(Player _player)
     {
+        color = CustomPropertyCode.COLORS[(int)_player.CustomProperties[CustomPropertyCode.TEAM_CODE]];
         player = _player;
-        playerColor.color = CustomPropertyCode.COLORS[(int)_player.CustomProperties[CustomPropertyCode.TEAM_CODE]];
+        playerColor.color = new Color(color.r, color.g, color.b, 0.7f);
+        winnerIcon.color = color;
+        playerNameText.color = color;
         playerNameText.text = player.NickName;
     }
 }
