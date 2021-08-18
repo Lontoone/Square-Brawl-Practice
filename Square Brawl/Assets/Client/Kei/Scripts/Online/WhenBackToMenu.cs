3cddd0935834c032a944f9967b27a8f6925f5361
cingjia <news2000tw@gmail.com>
2021-08-18 19:33:24 +0900

    add back with connect

---
+++ Square Brawl/Assets/Client/Kei/Scripts/Online/WhenBackToMenu.cs
@@ -0,0 +1,23 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WhenBackToMenu : MonoBehaviour
+{
+    public UnityEvent BackWithConnectedEvent;
+    public UnityEvent BackWithDisconnectedEvent;
+    void Start()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            BackWithConnectedEvent?.Invoke();
+        }
+        else
+        {
+            BackWithDisconnectedEvent?.Invoke();
+        }
+    }
+
+}
