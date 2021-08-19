616a33058990d6dc2c11a427dfc36246a9b7c262
sean <sean0990461@gmail.com>
2021-08-19 14:59:46 +0900

    control amend

---
+++ Square Brawl/Assets/Client/Sean/mockup/ArrorButton.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+
+public class ArrorButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
+{
+    private Animator animator;
+    private Button button;
+    private bool inButton = false;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(delegate { OnPress(); });
+    }
+
+    private void OnPress()
+    {
+        animator.Play("Pressed");
+    }
+
+    private void Highlighted()
+    {
+        animator.Play("Highlighted");
+    }
+
+    private IEnumerator Idle()
+    {
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorClipInfo(0)[0].clip.name != "Pressed");
+        animator.Play("Exit");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        inButton = true;
+        Highlighted();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StartCoroutine(Idle());
+        inButton = false;
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        inButton = true;
+        Highlighted();
+    }
+    public void OnDeselect(BaseEventData eventData)
+    {
+        StartCoroutine(Idle());
+        inButton = false;
+    }
+}
